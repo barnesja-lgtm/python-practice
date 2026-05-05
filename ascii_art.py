5468e1c6861cfdbc294777418b7ab59a7eb2008d
barnesja-lgtm <barnesja@wsc.school.nz>
2026-05-05 15:03:13 +1200

    Create ascii_art.py

---
+++ ascii_art.py
@@ -0,0 +1,13 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+
+print ("ttttttt hhh   hhh eeeee     eeeee nnn    nn ddddd")
+print ("  tt    hhh   hhh ee        ee    nnnnn  nn dd  ddd")
+print ("  tt    hhhhhhhhh eeeee     eeeee nn  nn nn dd   dd")
+print ("  tt    hhh   hhh ee        ee    nn   nnnn dd  ddd")
+print ("  tt    hhh   hhh eeeee     eeeee nn    nnn ddddd")
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
